bd7dcd2b76521aae3d2d79c17351ed314c8f0981
Louis Ducher <louis.ducher@epfedu.fr>
2025-12-17 15:36:54 +0100

    fonction cheval et lumiere

--- Chevauchée Fantastique/src/Cellule.java
+++ Chevauchée Fantastique/src/Cellule.java
@@ -8,5 +8,30 @@
  * @author paulh
  */
 public class Cellule {
-    
+private boolean allumee;
+private boolean chevalPresent;
+
+public Cellule(boolean allumee){
+    this.allumee=allumee;
+    this.chevalPresent= false;
+}
+ public boolean entrer() {
+        if (!allumee) {
+            return false;
+        }
+        allumee = false;      
+        chevalPresent = true;
+        return true;
+    }
+  public void sortir() {
+        chevalPresent = false;
+    }
+
+    public boolean estAllumee() {
+        return allumee;
+    }
+
+    public boolean aCheval() {
+        return chevalPresent;
+    }
 }
