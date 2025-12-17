443f07a23c2615b064ae850d2a9e28fb59e057e6
PaulHERMANN <paul.hermann@epfedu.fr>
2025-12-17 15:01:36 +0100

    Du plateau vers la grill v2

---
+++ Chevauchée Fantastique/src/JbuttonAmeliore.java
@@ -0,0 +1,21 @@
+
+import javax.swing.JButton;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author paulh
+ */
+public class JbuttonAmeliore extends JButton {
+    int cooX;
+    int cooY;
+
+    JbuttonAmeliore(int cooX, int cooY) {
+        this.cooX=cooX;
+        this.cooY=cooY;
+    }
+}
