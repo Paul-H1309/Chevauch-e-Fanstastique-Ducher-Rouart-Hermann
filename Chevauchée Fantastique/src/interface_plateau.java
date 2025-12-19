c643f4b008c0d04621956074d7953b2f0b4a8124
PaulHERMANN <paul.hermann@epfedu.fr>
2025-12-19 15:00:31 +0100

    interface débug

--- Chevauchée Fantastique/src/interface_plateau.java
+++ Chevauchée Fantastique/src/interface_plateau.java
@@ -173,6 +173,7 @@ public class interface_plateau extends javax.swing.JFrame {
         // TODO add your handling code here:
         Menu_Jeu f = new Menu_Jeu(); 
         f.setVisible(true) ; 
+        this.dispose();
     }//GEN-LAST:event_Bt_menuActionPerformed
 
     /**
