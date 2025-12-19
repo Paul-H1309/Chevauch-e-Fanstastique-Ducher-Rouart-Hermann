fea9a4581b2af7812724de45b15a62482cee423c
Louis Ducher <louis.ducher@epfedu.fr>
2025-12-19 15:47:03 +0100

    generer les cases eteintes v3

--- Chevauchée Fantastique/src/metier.java
+++ Chevauchée Fantastique/src/metier.java
@@ -4,11 +4,27 @@ import java.util.ArrayList;
 public class metier {
 
     int TAILLE = 5;
-    
     private int chevalX;
     private int chevalY;
-
     Cellule[][] grille = new Cellule[TAILLE][TAILLE];
+
+    private int[][][] tours = {
+        {
+            {0,0},{2,1},{4,0},{3,2},{4,4},
+            {2,3},{0,4},{1,2},{0,0},{1,4},
+            {3,3},{4,1},{2,0},{0,1},{1,3},
+            {3,4},{4,2},{3,0},{1,1},{0,2},
+            {2,2},{4,3},{3,1},{1,0},{2,4}
+        },
+        {
+            {2,2},{0,1},{1,3},{0,4},{2,3},
+            {4,4},{3,2},{4,0},{2,1},{0,0},
+            {1,2},{0,4},{1,0},{3,1},{4,3},
+            {2,4},{0,3},{1,1},{3,0},{4,2},
+            {3,4},{1,3},{0,2},{2,0},{4,1}
+        }
+  
+    };
 
     public metier() {
 
@@ -20,13 +36,61 @@ public class metier {
 
         initialisation();
 
-        chevalX = 2;
-        chevalY = 2;
+
+        int[][] tour = choisirTourAleatoire();
+
+
+        int lastIndex = tour.length - 1;
+        chevalX = tour[lastIndex][0];
+        chevalY = tour[lastIndex][1];
         grille[chevalX][chevalY].placerCheval();
     }
 
-  
-  /*
+ 
+    private void initialisation() {
+        for (int i = 0; i < TAILLE; i++) {
+            for (int j = 0; j < TAILLE; j++) {
+                grille[i][j].allumer();
+            }
+        }
+    }
+
+
+    public boolean deplacerCheval(int xDest, int yDest) {
+        if (xDest < 0 || xDest >= TAILLE || yDest < 0 || yDest >= TAILLE) {
+            return false;
+        }
+
+        int dx = Math.abs(xDest - chevalX);
+        int dy = Math.abs(yDest - chevalY);
+
+        if (!((dx == 2 && dy == 1) || (dx == 1 && dy == 2))) {
+            return false;
+        }
+
+        grille[chevalX][chevalY].retirerCheval();
+
+        chevalX = xDest;
+        chevalY = yDest;
+
+        grille[chevalX][chevalY].placerCheval();
+
+       
+        grille[chevalX][chevalY].eteindre();
+
+        return true;
+    }
+
+    public Cellule[][] getGrille() {
+        return grille;
+    }
+
+    public int[][] choisirTourAleatoire() {
+        int index = (int)(Math.random() * tours.length);
+        return tours[index];
+    }
+}
+/*
     public boolean deplacerCheval(int xDest, int yDest) {
 
         if (xDest < 0 || xDest >= TAILLE || yDest < 0 || yDest >= TAILLE) {
@@ -58,58 +122,3 @@ public class metier {
     }
 /*/
    
-    private void initialisation() {
-    boolean[][] visite = new boolean[TAILLE][TAILLE];
-    int[][] parcours = new int[TAILLE * TAILLE][2];
-
-    int xFin = (int)(Math.random() * TAILLE);
-    int yFin = (int)(Math.random() * TAILLE);
-
-    parcours[0][0] = xFin;
-    parcours[0][1] = yFin;
-    visite[xFin][yFin] = true;
-
-    for (int i = 1; i < TAILLE * TAILLE; i++) {
-        int[] voisins = getVoisinsNonVisites(parcours[i-1][0], parcours[i-1][1], visite);
-        if (voisins == null) {
-            i = 0;
-            visite = new boolean[TAILLE][TAILLE];
-            parcours[0][0] = xFin;
-            parcours[0][1] = yFin;
-            visite[xFin][yFin] = true;
-        } else {
-            parcours[i][0] = voisins[0];
-            parcours[i][1] = voisins[1];
-            visite[voisins[0]][voisins[1]] = true;
-        }
-    }
-
-
-    for (int i = 0; i < TAILLE; i++) {
-        for (int j = 0; j < TAILLE; j++) {
-            grille[i][j].allumer();
-        }
-    }
-    chevalX = parcours[TAILLE*TAILLE - 1][0];
-    chevalY = parcours[TAILLE*TAILLE - 1][1];
-    grille[chevalX][chevalY].placerCheval();
-}
-
-private int[] getVoisinsNonVisites(int x, int y, boolean[][] visite) {
-    int[][] moves = {{2,1},{1,2},{-1,2},{-2,1},{-2,-1},{-1,-2},{1,-2},{2,-1}};
-    ArrayList<int[]> candidats = new ArrayList<>();
-    for (int[] m : moves) {
-        int nx = x + m[0];
-        int ny = y + m[1];
-        if (nx >= 0 && nx < TAILLE && ny >= 0 && ny < TAILLE && !visite[nx][ny]) {
-            candidats.add(new int[]{nx, ny});
-        }
-    }
-    if (candidats.isEmpty()) return null;
-    return candidats.get((int)(Math.random() * candidats.size()));
-}
-
-    public Cellule[][] getGrille() {
-        return grille;
-    }
-}
